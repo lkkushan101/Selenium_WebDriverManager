701b6291415fb93fd3e9a6e9217c0b169e495a99
lkkushan101 <kam@codehousegroup.com>
2018-06-05 12:00:59 +0530

    v3

--- automa/selenium_python/test.py
+++ automa/selenium_python/test.py
@@ -1,26 +1,18 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonOrgSearch(unittest.TestCase):
+driver = webdriver.Chrome()
+driver.get("http://www.google.lk")
+assert "Google" in driver.title
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
+search_input = driver.find_element_by_id("lst-ib")
+search_input.send_keys("Selenium")
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("http://www.google.lk")
-        self.assertIn("Google", driver.title)
-        elemntSearchInput = driver.find_element_by_id("lst-ib")
-        elemntSearchInput.send_keys("Selenium")
-        elemntSearchButton = driver.find_element_by_name("btnK")
-        elemntSearchButton.click()
-        selenium_link = driver.find_element_by_link_text("Selenium - Web Browser Automation")
-        selenium_link.click();
-        self.assertIn("Selenium - Web Browser Automation", driver.title)
+search_button = driver.find_element_by_name("btnK")
+search_button.click()
 
-    def tearDown(self):
-        self.driver.close()
+selenium_link = driver.find_element_by_link_text("Selenium - Web Browser Automation")
+selenium_link.click();
 
-if __name__ == "__main__":
-    unittest.main()
+assert "Selenium - Web Browser Automation" in driver.title
+driver.close()
