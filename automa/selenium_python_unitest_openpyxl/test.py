eed52f55d7e2cac59bccf9bf4a1f4f19c61dbb79
lkkushan101 <kam@codehousegroup.com>
2018-06-05 13:45:30 +0530

    V1

---
+++ automa/selenium_python_unitest_openpyxl/test.py
@@ -0,0 +1,37 @@
+import unittest
+from selenium import webdriver
+from openpyxl import load_workbook
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearch(unittest.TestCase):
+ 
+     
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+          
+        
+        
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        wb = load_workbook('c:/python_data.xlsx')
+        # Loading the excel workbook
+        sheet = wb.get_sheet_by_name('Sheet1')
+        # Loading the excel sheet by name 
+        driver.get( sheet['A2'].value)
+        # Loading the URL from Excel
+        self.assertIn("Google", driver.title)
+        elemntSearchInput = driver.find_element_by_id("lst-ib")
+        elemntSearchInput.send_keys( sheet['B2'].value)
+        # Loading the Search Word from Excel
+        elemntSearchButton = driver.find_element_by_name("btnK")
+        elemntSearchButton.click()
+        selenium_link = driver.find_element_by_link_text(sheet['C2'].value)
+        selenium_link.click();
+        self.assertIn(sheet['C2'].value, driver.title)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
