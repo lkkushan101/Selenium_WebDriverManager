81dbaddc50a1449f4aa169527253bdc7fee4c735
lkkushan101 <kam@codehousegroup.com>
2018-06-10 19:22:11 +0530

    v1

---
+++ eclipse-workspace-workshop/KarateDSL/src/test/java/features/karateRunner.java
@@ -0,0 +1,14 @@
+package features;
+import org.junit.runner.RunWith;
+import com.intuit.karate.junit4.Karate;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Karate.class)
+
+public class karateRunner {
+
+}
+
+
+ 
+
