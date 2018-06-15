ca6ef7c1eabbe888218c2ddd7dcf00d8e08c3668
lkkushan101 <kam@codehousegroup.com>
2018-06-15 22:00:38 +0530

    v1

---
+++ selenium_tryouts/SeleniumBasicAngular/src/main/java/SeleniumSAP/Selenium_SAP.java
@@ -0,0 +1,73 @@
+package SeleniumSAP;
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class Selenium_SAP {
+	
+	WebDriver driver;
+	
+@BeforeTest
+	public void setup()
+	{
+		
+		System.setProperty("webdriver.chrome.driver","C:\\chromedrv\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		 driver.get("https://angularjs.org/"); // sample url	
+         	
+	}
+
+@Test
+
+	public void testSearch() throws InterruptedException
+	{
+	    WebDriverWait wait=new WebDriverWait(driver, 20);  	
+	
+		
+		WebElement namesInput;
+		namesInput= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//input[@ng-model='yourName']")));
+		namesInput.sendKeys("Kushan");
+		
+		WebElement todo_item= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//input[@ng-model='todoList.todoText']")));
+		todo_item.sendKeys("Kushan Amarasiri");
+		
+		WebElement button_add= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//input[@class='btn-primary']")));
+		button_add.click();
+		
+		
+		
+		String name = driver.findElement(By.xpath("/html/body/div[2]/div[3]/div[2]/div[2]/div/ul/li[3]/label/span")).getText();
+		
+		assertEquals(name,"Kushan Amarasiri" );
+		
+		
+		WebElement search_input= wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath( "//input[@ng-model='projectList.search']")));
+		search_input.sendKeys("AngularJS");
+		
+		
+		if (driver.findElement(By.linkText("AngularJS")).isDisplayed())
+		{
+			System.out.println("Item found");
+		}
+		
+				
+		
+		
+		
+	}
+
+@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+		driver.quit();
+	}
+}
